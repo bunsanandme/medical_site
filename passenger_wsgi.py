0dda6b680867cd1c4bbeae6b1467d409c162b282
Daniil Belov <bunsanandme@yandex.ru>
2022-11-30 14:16:31 +0300

    Delete passenger_wsgi.py

--- passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import os, sys
-sys.path.insert(0, '/var/www/u1696195/data/www/roboticreg.ru/medical_site')
-sys.path.insert(1, '/var/www/u1696195/data/djangoenv/lib/python3.10/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'medical_site.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
